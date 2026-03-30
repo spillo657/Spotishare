0d3770a099a27d8f7c68f645db684ea3963e6802
spillo657 <mattiasanfilippo45@gmail.com>
2026-03-30 17:30:35 +0200

    Forzato redirect dinamico

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic"; // Forza Next.js a non usare la memoria vecchia
+
 export default function Home() {
   redirect("/login");
 }
